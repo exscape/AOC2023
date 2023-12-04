e59832ade634adb1bd6c21ea4d220971cf5340c8
Thomas Backman <serenity@exscape.org>
2023-12-04 12:42:30 +0100

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -5,14 +5,30 @@ NUMBER_REGEX = re.compile(r'\d+')
 if __name__=='__main__':
     lines = open('data/day4.txt').read().splitlines()
 
-    sum = 0
+    card_match_count = {} # Map of card ID -> # of matching numbers on that card
+    card_instances = {} # Map of card ID -> # of copies we have of that card
+
+    # Parse cards and calculate score for part 1
+    score = 0
     for line in lines:
-        _, winning, my = re.split('[:|]', line)
+        card, winning, my = re.split('[:|]', line)
+        card_number = int(NUMBER_REGEX.search(card).group(0))
         winning_numbers = NUMBER_REGEX.findall(winning)
         my_numbers = NUMBER_REGEX.findall(my)
 
-        num_winning = len(set(my_numbers) & set(winning_numbers))
-        score = 2**(num_winning - 1) if num_winning > 0 else 0
-        sum += score
+        num_matches = len(set(my_numbers) & set(winning_numbers))
+        card_match_count[card_number] = num_matches
+        card_instances[card_number] = 1
+        card_score = 2**(num_matches - 1) if num_matches > 0 else 0
+        score += card_score
     
-    print(f"Part 1: {sum}")
+    print(f"Part 1: {score}")
+
+    # Calculate card instances for part 2
+    for card, num_matches in card_match_count.items():
+        for instance in range(card_instances[card]):
+            # num_matches matches gives one copy of each card in the range card + 1 to card + num_matches (inclusive)
+            for c in range(card + 1, card + num_matches + 1):
+                card_instances[c] += 1
+
+    print(f"Part 2: {sum(card_instances.values())}")

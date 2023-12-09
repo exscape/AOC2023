da7bf6cee8c1871e4b7a9259745d95d315b2e873
Thomas Backman <serenity@exscape.org>
2023-12-09 12:31:56 +0100

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -1,26 +1,28 @@
 import itertools
+from collections import deque
+
+def extrapolate_history(history):
+    # Generate a list of differences: [[1, 3, 6, 10, 15, 21], [2, 3, 4, 5, 6], [1, 1, 1, 1], [0, 0, 0]]
+    # diffs[0] contains the original values, followed by each set of differences in turn
+    diffs = [deque(history)]
+    while not all([x == 0 for x in diffs[-1]]):
+        diffs.append(deque([b-a for (a, b) in itertools.pairwise(diffs[-1])]))
+
+    # Work backwards through the lists adding up values
+    for current, previous in itertools.pairwise(reversed(diffs)):
+        previous.append(current[-1] + previous[-1])
+        previous.appendleft(previous[0] - current[0])
+
+    return diffs[0]
 
 def solve(histories):
-    last_values = []
-    for history in histories:
-        # Generate a list of differences: [[1, 3, 6, 10, 15, 21], [2, 3, 4, 5, 6], [1, 1, 1, 1], [0, 0, 0]]
-        diffs = [history]
-        while not all([x == 0 for x in diffs[-1]]):
-            diffs.append([b-a for (a, b) in itertools.pairwise(diffs[-1])])
-
-        diffs[-1].append(0) # TODO: is this really necessary? I can't see how
-
-        # Start working backwards through the lists
-        for i in range(-1, -len(diffs), -1):
-            # Append to the previous list: last value of this list + last value of the previous list
-            diffs[i-1].append(diffs[i][-1] + diffs[i-1][-1])
-
-        last_values.append(diffs[0][-1])
-    return sum(last_values)
+    histories = [extrapolate_history(history) for history in histories]
+    return [sum([h[-1] for h in histories]), sum([h[0] for h in histories])]
 
 if __name__=='__main__':
     lines = open('data/day9.txt').read().splitlines()
     histories = [[int(n) for n in history] for history in [line.split(" ") for line in lines]]
 
-    answer = solve(histories)
-    print(f"Part 1: {answer}")
+    (part1, part2) = solve(histories)
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")

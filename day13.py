43be221a763b49de0894b5b19d35656e8abde028
Thomas Backman <serenity@exscape.org>
2023-12-13 17:54:37 +0100

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,34 +1,43 @@
 import itertools
 from common import CharacterGrid
 
-def find_reflection(line_func, line_count):
-    # Check for reflected lines *above* this index, which always leaves at least one line on either side of the reflection axis
-    # TODO: better limits, this goes out of bounds to figure out where to stop
+def find_reflection(line_func, line_count, smudge = False):
+    def differences(line_a, line_b):
+        """ How many characters differ in these strings? """
+        return len(line_a) - sum(1 for i in range(len(line_a)) if line_a[i] == line_b[i])
+
     for reflection_line in range(1, line_count):
-        for offset in range(0, line_count):
-            if (a:=line_func(reflection_line - offset - 1)) != (b:=line_func(reflection_line + offset)) \
-                and not (all(x==None for x in a) or all(x==None for x in b)):
-                # These don't match, AND neither of the lines is out of bounds, so there's no reflection here, try another line
-                break
-            if all(x==None for x in a) and all(x==None for x in b):
-                # We reached outside the bounds on both sides, without finding a difference, so we found the reflection line
+        misses = 0
+        for offset in range(0, line_count // 2):
+            a = reflection_line - offset - 1
+            b = reflection_line + offset
+            if a >= 0 and b < line_count:
+                line_a = line_func(a)
+                line_b = line_func(b)
+                if smudge and differences(line_a, line_b) == 1:
+                    # This didn't match, but there was exactly one single mismatch so far, so keep going
+                    misses += 1
+                elif line_a != line_b:
+                    break
+        else:
+            # The loop finished without breaking, so this could be a match
+            if not smudge or misses == 1:
                 return reflection_line
 
     return 0
 
-def solve(grids):
-    grids = [CharacterGrid(grid_data, default_value=None) for grid_data in grids]
-
+def solve(grids, smudge):
     sum = 0
     for grid in grids:
-        sum += 100 * (reflection_line := find_reflection(grid.row, grid.row_count))
+        sum += 100 * (reflection_line := find_reflection(grid.row, grid.row_count, smudge))
         if reflection_line == 0:
-            sum += find_reflection(grid.col, grid.col_count)
+            sum += find_reflection(grid.col, grid.col_count, smudge)
 
     return sum
 
 if __name__=='__main__':
     lines = open('data/day13.txt').read().splitlines()
-    grids = [list(g) for k, g in itertools.groupby(lines, bool) if k]
+    grids = [CharacterGrid(list(g), default_value=None) for k, g in itertools.groupby(lines, bool) if k]
 
-    print(f"Part 1: {solve(grids)}")
+    print(f"Part 1: {solve(grids, smudge=False)}")
+    print(f"Part 2: {solve(grids, smudge=True)}")
